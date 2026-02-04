4b3bcdb9f408316e758c676a57442303fdebd3b9
Prachi Sharnagat <prachisharnagat06@gmail.com>
2026-02-04 22:24:14 +0530

    majority element n/3

--- leetcode/array/018_majorityElementN3time.cpp
+++ leetcode/array/018_majorityElementN3time.cpp
@@ -38,7 +38,8 @@ vector<int> majorityElement_brute(vector<int> &arr)
 
     return vector<int>(st.begin(), st.end());
 }
-
+// tc -> o(n)
+// sc -> o(n)
 vector<int> majorityElement_better(vector<int> &arr){
     map<int,int> mpp;
     vector<int> list;
@@ -52,9 +53,51 @@ vector<int> majorityElement_better(vector<int> &arr){
         }
         if(list.size()==2) break;
     }
-
+// 2 array sort 2log2 
     sort(arr.begin(),arr.end());
     return list;
+}
+// tc -> o(2n)
+// sc -> o(2) // for storing 2 element 
+vector<int> majorityElement_optimal(vector<int> &nums){
+    vector<int> ans;
+    int n = nums.size();
+    int cnt1 = 0, cnt2 =0;
+    int elm1 = INT_MIN, elm2 = INT_MIN;
+    // int previous1 = elem1;
+
+    for(int i=0; i<n; i++){
+        if(cnt1 == 0 && nums[i]!= elm2){
+            cnt1 = 1;
+            elm1 = nums[i];
+        }
+        else if(cnt2==0 && nums[i]!=elm1){
+            cnt2 = 1;
+            elm2 = nums[i];
+        }
+        else if(elm1 == nums[i]) cnt1++;
+         else if(elm2 == nums[i]) cnt2++;
+         else{
+            cnt1--;
+            cnt2--;
+         }
+    }
+
+    int checkCount1 = 0, checkCount2 = 0;
+    for(int i=0; i<n; i++){
+        if(nums[i] == elm1)  checkCount1++;
+        else if(nums[i]==elm2) checkCount2++;
+    }
+    int mini = (n/3) + 1;
+    if(checkCount1 >= mini) {
+        ans.push_back(elm1);
+    }
+    if(checkCount2 >= mini){
+        ans.push_back(elm2);
+    }
+    // osrt 2log2
+    sort(ans.begin(),ans.end());
+    return ans;
 }
 
 
@@ -69,7 +112,8 @@ int main()
         cin >> arr[i];
     }
 
-    vector<int> ans = majorityElement_better(arr);
+    // vector<int> ans = majorityElement_better(arr);
+    vector<int> ans = majorityElement_optimal(arr);
      for (int i = 0; i < ans.size(); i++)
     {
         cout << ans[i] << " ";
