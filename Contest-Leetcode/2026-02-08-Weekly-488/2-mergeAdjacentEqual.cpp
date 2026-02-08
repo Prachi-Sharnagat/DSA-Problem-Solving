f89a8b7edbc9a642378a7513b56f14040532b6ee
Prachi Sharnagat <prachisharnagat06@gmail.com>
2026-02-08 10:41:39 +0530

    1. count dominant indices  2. merge adjacent equal

---
+++ Contest-Leetcode/2026-02-08-Weekly-488/2-mergeAdjacentEqual.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+  vector<long long> mergeAdjacentEqual_brute(vector<long long> &nums){
+
+
+
+    return vector<long long> (arr.begin(), arr.end());
+  }
+
+  vector<long long> mergeAdjacentEqual_better(vector<long long> &nums){
+
+
+
+  }
+
+
+int main(){
+    int size;
+    cin >> size;
+
+    vector<int> arr(size);
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+
+
+    
+    for(int i=0; i<arr.size(); i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+
+
+}
