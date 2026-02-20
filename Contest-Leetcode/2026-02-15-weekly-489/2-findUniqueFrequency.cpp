17475c0f3597b8491b8442ccdb9187c82ad59d2b
Prachi Sharnagat <prachisharnagat06@gmail.com>
2026-02-20 23:49:48 +0530

    find first unique frequencyCount

--- Contest-Leetcode/2026-02-15-weekly-489/2-findUniqueFrequency.cpp
+++ Contest-Leetcode/2026-02-15-weekly-489/2-findUniqueFrequency.cpp
@@ -1,8 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+// 20 20 10 30 
+// first unique count return 
 int firstUniqueFreq(vector<int>& nums) {
+   unordered_map<int,int> mpp;
+   unordered_mpp<int,int> frequencyCount;
 
+   for(int x:nums){
+    mpp[x]++;
+   }
+
+   for(int x:mpp){
+        frequencyCount[x.second]++;
+   }
+
+   for(int x:nums){
+    if(frequencyCount[mpp[x]]==1){
+        return x;
+    }
+   }
+   return -1;
 
 
 
