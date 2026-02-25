945dde275898864ef48c95b9ff1be847a2bf5181
Prachi Sharnagat <prachisharnagat06@gmail.com>
2026-02-25 23:43:58 +0530

    reverse string

---
+++ leetcode/array/035_reverseArray.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left = 0;
+        int right = s.size() - 1;
+
+        while (left < right) {
+            swap(s[left], s[right]);
+            left++;
+            right--;
+        }
+    }
+};
+
+int main() {
+
+    vector<char> s = {'h','e','l','l','o'};
+
+    Solution obj;
+    obj.reverseString(s);
+    cout << "[ ";
+    for(char c : s){
+        cout << "'" << c << "'" << " ";
+    }
+    cout << "]" << endl;
+
+    return 0;
+}
