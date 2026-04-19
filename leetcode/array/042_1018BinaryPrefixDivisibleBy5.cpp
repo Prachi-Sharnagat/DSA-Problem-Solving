4504e6f21e32e3fec2d70e3da8a318b203185409
Prachi Sharnagat <prachisharnagat06@gmail.com>
2026-04-19 14:23:40 +0530

    solved 1 que

---
+++ leetcode/array/042_1018BinaryPrefixDivisibleBy5.cpp
@@ -0,0 +1,19 @@
+
+     class Solution {
+public:
+    vector<bool> prefixesDivBy5(vector<int>& nums) {
+        vector<bool> ans;
+        int rem = 0; // divissible by 5
+        for(int i=0; i<nums.size(); i++){
+            rem = (rem*2 + nums[i])%5;
+            if(rem==0){
+                ans.push_back(true);
+            }
+            else{
+                ans.push_back(false);
+            }
+        }
+
+        return ans;
+    }
+};
