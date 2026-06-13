2f33cdae287277edb5e94fd90416fee6d9c4772a
Prachi Sharnagat <prachisharnagat06@gmail.com>
2026-06-13 17:15:17 +0530

    2 que sole

---
+++ leetcode/recursion/001_pwXN.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+
+        double ans = 1.0;
+        long long N = n;
+        if(N<0){
+            N = -N;
+              while(N>0){
+            ans *= x;
+            N--;
+        }
+            return 1/ans;
+        }
+        while(N>0){
+            ans *= x;
+            N--;
+        }
+
+        return ans;
+        
+    }
+};
---
+++ leetcode/string/012_weightedWordMapping.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans = "";
+        for(int i=0; i<words.size(); i++){
+             int total = 0;
+            for(char s : words[i]){
+               total += weights[s-'a'];
+            }
+            ans.push_back(122 - (total%26));
+            
+        }
+
+        return ans;
+    }
+};
