f1cb42acdb047a10c12b064a7aa5452fc452bbb1
Prachi Sharnagat <prachisharnagat06@gmail.com>
2026-07-13 22:29:16 +0530

    1 que

---
+++ QueOfDay_leetcode/010_sequentialNum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        vector<int> ans;
+        string s = "123456789";
+
+        for (int len = 2; len <= 9; len++) {          // length of number
+            for (int i = 0; i + len <= 9; i++) {      // starting index
+                int num = stoi(s.substr(i, len));
+
+                if (num >= low && num <= high)
+                    ans.push_back(num);
+            }
+        }
+
+        return ans;
+    }
+};
