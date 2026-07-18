4137f2c8ee90ae379db065d40a5c99e92dd6a47b
Prachi Sharnagat <prachisharnagat06@gmail.com>
2026-07-18 21:36:42 +0530

    1 que

---
+++ QueOfDay_leetcode/015_findGcd.cpp
@@ -0,0 +1,8 @@
+#include<numeric>
+class Solution {
+public:
+    int findGCD(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return std::gcd(nums[0], nums[nums.size()-1]);     
+    }
+};
