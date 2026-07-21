0bee65886e4afdd7711e5472c628a68909ea3199
Prachi Sharnagat <prachisharnagat06@gmail.com>
2026-07-21 22:54:49 +0530

    1 que

---
+++ leetcode/binaryTree/006_cntDominant.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right)
+ *         : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int dominant = 0;
+
+    int dfs(TreeNode* root) {
+        if (root == NULL)
+            return INT_MIN;
+
+        int leftMax = dfs(root->left);
+        int rightMax = dfs(root->right);
+
+        if (root->val >= leftMax && root->val >= rightMax)
+            dominant++;
+
+        return max(root->val, max(leftMax, rightMax));
+    }
+
+    int countDominantNodes(TreeNode* root) {
+        dfs(root);
+        return dominant;
+    }
+};
