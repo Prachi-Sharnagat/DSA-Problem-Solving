75d2966dd1a9452dc01bbe58531d724d44b4022f
Prachi Sharnagat <prachisharnagat06@gmail.com>
2026-07-23 23:54:18 +0530

    1  que

---
+++ revision/002_rotateArrayRightByK.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+
+    void rotate(vector<int>& nums, int k) {
+        if(nums.empty()) {
+            return ;
+        }
+        int n = nums.size();
+        k = k%n;
+
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+    }
+};
+
+
+class Solution {
+public:
+    void reverse(vector<int> &nums, int start , int end){
+        while(start<=end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++; 
+            end--;
+        }
+    }
+    void rotate(vector<int>& nums, int k) {
+        if(nums.empty()) {
+            return ;
+        }
+        int n = nums.size();
+        k = k%n;
+
+        reverse(nums,0, n-1);
+        for(auto num : nums){
+            cout << num << " ";
+        }
+        reverse(nums,0, k-1);
+        reverse(nums,k,n-1);
+        
+
+    }
+};
